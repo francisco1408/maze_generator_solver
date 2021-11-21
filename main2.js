1cfb9d207cae7cdb2fb7b1b4ae85e392ec9a5b0b
Francisco Valdes <fco1408@outlook.com>
2021-11-21 15:20:20 -0600

    integrated the maze library to the second interface

--- main2.js
+++ main2.js
@@ -123,3 +123,24 @@ scrollAnimations.component_configurations = [
     {"component_list":section2_title, "configuration_key":"section2_title"},
 ];
 scrollAnimations.setScrollEvent("scroll-container");
+//Sets maze
+let mazes = [];
+let mazeCount = 0;
+document.getElementById("generate").addEventListener("click",()=>{
+    let rows = parseInt(document.getElementById("rows").value);
+    let columns = parseInt(document.getElementById("columns").value);
+    let maze1 = new maze(rows,columns,mazeCount);
+    mazeCount++;
+    maze1.render("maze1");
+    maze1.givePercentages(rows,columns);
+    mazes.push(maze1);
+    maze1.generateMaze();
+    
+});
+//Solver button
+let solver_button = document.getElementById("solve");
+solver_button.addEventListener("click",()=>{
+    console.log("click");
+    let _solver = new solver(mazes.length - 1);
+    _solver = null;
+})
